cd969d5273b600429cb0bcffd50831822d2cb526
jhyunwoo <tiers.edamame0z@icloud.com>
2022-03-29 14:16:31 +0900

    c study 2022.03.29

--- starmaker.c
+++ starmaker.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
 
 int main(void){
-    int i, j;
+    int a, i, j, k;
 
-    for(i=1;i<=5;i++){
-        for(j=1; j <= i; j ++){
+    printf("roop time: ");
+    scanf("%d", &a);
+
+    for(i = 1; i <= a; i++){
+        for(j=1; j <= a - i; j++){
+            printf(" ");
+        }
+        for(k = 1; k <= 2*i-1; k++){
             printf("*");
         }
+
         printf("\n");
     }
 }
