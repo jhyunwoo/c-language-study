2785239394d2d9e3b0614db0dc2c3a2b155ca3f0
jhyunwoo <tiers.edamame0z@icloud.com>
2022-05-24 13:34:00 +0900

    2022.05.24

--- example_10-5.c
+++ example_10-5.c
@@ -4,24 +4,24 @@ void CountIncrement2(int *n);
 
     
 
-    int main(void){
-        int a = 10;
-        printf("a의 초깃값 : %d\n", a);
+int main(void){
+    int a = 10;
+    printf("a의 초깃값 : %d\n", a);
 
-        CountIncrement1(a);
-        printf("CountIncrement1 함수 실행 후 a의 값 : %d\n", a);
+    CountIncrement1(a);
+    printf("CountIncrement1 함수 실행 후 a의 값 : %d\n", a);
 
-        CountIncrement2(&a);
-        printf("CountIncrement2 함수 실행 후 a의 값 : %d\n", a);
+    CountIncrement2(&a);
+    printf("CountIncrement2 함수 실행 후 a의 값 : %d\n", a);
 
-        return 0;
-    }
+     return 0;
+}
 
-    void CountIncrement1(int n){
-        n++;
-    }
-    void CountIncrement2(int *n){
-        (*n)++;
-    }
+void CountIncrement1(int n){
+    n++;
+}
+void CountIncrement2(int *n){
+    (*n)++;
+}
 
     
