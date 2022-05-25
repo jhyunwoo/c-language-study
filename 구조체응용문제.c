a83518f892abbf78b44cf6cbf5fdaa08d3aca432
jhyunwoo <tiers.edamame0z@icloud.com>
2022-05-25 14:15:04 +0900

    구조체 응용 문제

--- 구조체응용문제.c
+++ 구조체응용문제.c
@@ -12,6 +12,6 @@ int main(void){
 
     scanf("%c %c %d", &u1.name, &u1.gender, &u1.age);
 
-    printf("st1객체는 이름이 %c, 성별이 %c, 나이가 %d이다.\n", u1.name, u1.gender, u1.age);
+    printf("st1 객체는 이름이 %c, 성별이 %c, 나이가 %d이다.\n", u1.name, u1.gender, u1.age);
     return 0;
 }
